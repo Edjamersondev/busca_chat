fff09a010f8029efa76ec7db90bbee5e2bbbf5a5
Edjamersondev <edjamerson48256@gmail.com>
2026-03-28 22:23:55 -0300

    adicionei comentarios no app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from fastapi import FastAPI
-from processamento import adicionar_documento, buscar
-from processamento import responder
+from fastapi import FastAPI #importei a biblioteca fastapi
+from processamento import adicionar_documento, buscar #importei as funçoes que criei no arquivo processamento
+from processamento import responder #importei a funçao que criei no arquivo processamento
 
-app = FastAPI()
+app = FastAPI() #criei uma aplicação chamada app com a classe principal FastAPI
 
-@app.get("/")
+@app.get("/") #criei uma rota  chamada por padrão
 def home():
     return {"mensagem": "API funcionando 🚀"}
 
-@app.post("/documento")
+@app.post("/documento") #criei uma rota chamada documento para adicionar textos
 def add_doc(texto: str):
     adicionar_documento(texto)
-    return {"status": "Documento adicionado"}
+    return {"status": "Texto adicionado"}
 
-@app.get("/buscar")
+@app.get("/buscar") #criei uma rota para buscar textos
 def busca(q: str):
     resultados = buscar(q)
     return {"resultados": resultados}
 
-@app.get("/pergunta")
+@app.get("/pergunta") #criei uma rota para perguntar sobre os textos 
 def perguntar(q: str):
     resposta = responder(q)
     return {"resposta": resposta}
